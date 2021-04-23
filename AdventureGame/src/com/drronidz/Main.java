ed84367b85ee6a24e7d839d4a96ea837297b1516
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-23 17:53:15 +0100

    issue done. closes #9

--- AdventureGame/src/com/drronidz/Main.java
+++ AdventureGame/src/com/drronidz/Main.java
@@ -22,13 +22,41 @@ public class Main {
         // Single letter commands (N, W, S, E, Q) should still be available.
 
         Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        locations.put(0, new Location(0, "You are sitting in the front of a computer learning Java", tempExit));
 
-        locations.put(0, new Location(0, "You are sitting in the front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road "));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        // Room One
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road ", tempExit));
+
+        // Room Two
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
+
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+
+
+        // Room Three
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building", tempExit));
+
+
+        // Room Four
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
+
+        // Room Five
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest", tempExit));
+
 
         Map<String, String> vocabulary = new HashMap<String, String>();
 
@@ -38,34 +66,10 @@ public class Main {
         vocabulary.put("EAST","E");
         vocabulary.put("WEST","E");
 
-        // Room One
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-        //locations.get(1).addExit("Q", 0);
-
-        // Room Two
-        locations.get(2).addExit("N", 5);
-        //locations.get(2).addExit("Q", 0);
-
-        // Room Three
-        locations.get(3).addExit("W", 1);
-        //locations.get(3).addExit("Q", 0);
-
-        // Room Four
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-        //locations.get(4).addExit("Q", 0);
-
-        // Room Five
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-        //locations.get(5).addExit("Q", 0);
-
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
             if(loc == 0) {
                 break;
             }
