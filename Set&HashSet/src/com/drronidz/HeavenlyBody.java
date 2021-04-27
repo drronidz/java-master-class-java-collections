da46656d26c151a234c226de08382b6a96744d9e
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-27 18:31:07 +0100

    issue done. closes #12.

--- Set&HashSet/src/com/drronidz/HeavenlyBody.java
+++ Set&HashSet/src/com/drronidz/HeavenlyBody.java
@@ -50,4 +50,10 @@ public class HeavenlyBody {
         String objName = ((HeavenlyBody) obj).getName();
         return this.name.equals(objName);
     }
+
+    @Override
+    public int hashCode() {
+        System.out.println("hashcode called");
+        return this.name.hashCode() + 57;
+    }
 }
