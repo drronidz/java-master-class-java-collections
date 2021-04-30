e5ab7d1994ae53588aa89c13381860c82bf1f48c
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-30 13:45:02 +0100

    issue done. closes #16.

--- Symmetric&Asymmetric/src/com/drronidz/Main.java
+++ Symmetric&Asymmetric/src/com/drronidz/Main.java
@@ -57,6 +57,43 @@ public class Main {
         String [] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
         divine.addAll(Arrays.asList(divineWords));
 
+        System.out.println("#####################################################");
+        System.out.println("nature - divine:");
+        Set<String> diffOne = new HashSet<>(nature);
+        diffOne.removeAll(divine);
+        printSet(diffOne);
 
+        System.out.println("#####################################################");
+        System.out.println("divine - nature:");
+        Set<String> diffTwo = new HashSet<>(divine);
+        diffTwo.removeAll(nature);
+        printSet(diffTwo);
+
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if(nature.containsAll(divine)) {
+            System.out.println("divine is a subset of nature");
+        }
+        if(nature.containsAll(intersectionTest)){
+            System.out.println("intersection is a subset of nature");
+        }
+        if(divine.containsAll(intersectionTest)){
+            System.out.println("intersection is subset of divine");
+        }
+    }
+
+    private static void printSet(Set<String> set) {
+        System.out.print("\t");
+        for (String s: set) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 }
