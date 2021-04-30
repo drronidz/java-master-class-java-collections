584816fe7f12ed570b614ef01d1930021557e61d
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-30 16:40:41 +0100

    issue done. closes #21.

---
+++ SortedCollections/src/com/drronidz/StockList.java
@@ -0,0 +1,67 @@
+package com.drronidz;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 4/30/2021 , 
+    CREATED ON : 3:51 PM
+*/
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList(Map<String, StockItem> list) {
+        this.list = new HashMap<>();
+    }
+
+    public StockItem get(String key){
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> items(){
+        return Collections.unmodifiableMap(list);
+    }
+
+    public int addStock(StockItem item) {
+        if(item != null) {
+            // check if already have quantities of this item
+               StockItem inStock = list.getOrDefault(item.getName(), item);
+            // StockItem inStock = list.get(item.getName());
+            // If there are already on this item, adjust the quantity
+            if(inStock != item){
+                item.adjustStock(inStock.getQuantityStock());
+            }
+            list.put(item.getName(), item);
+
+            return item.getQuantityStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity){
+        StockItem inStock = list.getOrDefault(item, null);
+        if((inStock != null) && (inStock.getQuantityStock() >= quantity) && (quantity >0)){
+            inStock.adjustStock(-quantity);
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item: list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.getQuantityStock();
+
+            s = s + stockItem + ". There are " + stockItem.getQuantityStock() + " in stock. Value of items: ";
+            s = s + itemValue + "\n";
+            totalCost += itemValue;
+        }
+
+        return s + "Total stock value " + totalCost;
+    }
+}
